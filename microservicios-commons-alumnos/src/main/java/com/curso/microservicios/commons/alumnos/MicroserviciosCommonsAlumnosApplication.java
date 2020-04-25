f7138b205cc61b964e19158eb907a7c7355dfb91
israel <israel.bejarano.a@gmail.com>
2020-04-25 13:06:41 +0200

    commit inicial

---
+++ microservicios-commons-alumnos/src/main/java/com/curso/microservicios/commons/alumnos/MicroserviciosCommonsAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.commons.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCommonsAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosCommonsAlumnosApplication.class, args);
+	}
+
+}
